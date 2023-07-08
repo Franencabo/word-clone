0f002e3b56751b936c0a33dfe09a7ba79b741c74
Fran <franencabo@gmail.com>
2023-07-08 07:59:21 +0200

    Exercise 1 : GuessInput

---
+++ src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form() {
+  return (
+    <form>
+      <input>'Hola'</input>
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log(guess);
+
+    setGuess("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        id="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        value={guess}
+        onChange={(event) => {
+          const nextGuess = event.target.value.toLocaleUpperCase();
+          setGuess(nextGuess);
+        }}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
